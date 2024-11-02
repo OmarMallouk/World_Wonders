0b5a57b40b3edfa3f85040ba47384c5cb5a0ba8a
OmarMallouk <mr.mallouk.om@gmail.com>
2024-11-02 15:02:13 +0200

    fetched more data and organized it in a card element

--- scripts/stats.js
+++ scripts/stats.js
@@ -12,6 +12,38 @@ const instance = axios.create({
       console.error("Error fetching wonders data:", error);
     }
   };
+
+
+  const displayWonders = (wonders) => {
+    const container = document.getElementById('card-container');
+    container.innerHTML = ''; // Clear the container if needed
+
+    wonders.forEach(wonder => {
+    const arrlinks = wonder.links?.length || "N/A";
+    const britannica = wonder.links?.britannica || "N/A";
+    const images = wonder.links?.images || [];
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+      const firstImage = images.length > 0 ? 
+      `<img src="${images[0]}" alt="Image of ${wonder.name}">`:
+      '<p>No images available.</p>';
+
+      card.innerHTML = `
+       <p><strong>Images:</strong></p>
+      ${firstImage}
+        <h3>${wonder.name}</h3>
+        <p><strong>Location:</strong> ${wonder.location}</p>
+        <p><strong>Description:</strong> ${wonder.summary}</p>
+  
+      
+      `;
+
+      container.appendChild(card);
+    });
+  };
+
+  fetchWonders();
   
 
  
