086f134dcbc52cb2af2ec3aa4f726ebba78014e2
OmarMallouk <mr.mallouk.om@gmail.com>
2024-11-02 18:44:12 +0200

    some fixes to the layout

--- scripts/page.js
+++ scripts/page.js
@@ -1,21 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('card-container');
-    const imageContainer = document.getElementById('image-container')
 
     const wonderData = JSON.parse(localStorage.getItem('selectedWonder'));
 
     if (wonderData) {
         const images = wonderData.links?.images || [];
-        const imageDetail = document.createElement('div');
-        imageDetail.classList.add('image-details')
+
 
         const cardDetail = document.createElement('div');
         cardDetail.classList.add('cards');
-
-        // imageDetail.innerHTML = `<img src="${images[0]}" alt="Image of ${wonderData.name}">`
-
         cardDetail.innerHTML = `
-        
+        <img src="${images[0]}" alt="Image of ${wonderData.name}">
       <h2>${wonderData.name}</h2>
       <h3><strong>Location:</strong> ${wonderData.location}</h3>
       <p>${wonderData.summary}</p>
