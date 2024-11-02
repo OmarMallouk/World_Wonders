183e35be799d7bee48c86d877df87f22dcb9df92
OmarMallouk <mr.mallouk.om@gmail.com>
2024-11-02 17:50:58 +0200

    added css properties

--- scripts/page.js
+++ scripts/page.js
@@ -1,21 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('card-container');
+    const imageContainer = document.getElementById('image-container')
 
     const wonderData = JSON.parse(localStorage.getItem('selectedWonder'));
 
     if (wonderData) {
         const images = wonderData.links?.images || [];
+        const imageDetail = document.createElement('div');
+        imageDetail.classList.add('image-details')
+
         const cardDetail = document.createElement('div');
-        cardDetail.classList.add('card-detail');
+        cardDetail.classList.add('cards');
+
+        // imageDetail.innerHTML = `<img src="${images[0]}" alt="Image of ${wonderData.name}">`
 
         cardDetail.innerHTML = `
-        <img src="${images[0]}" alt="Image of ${wonderData.name}">
+        
       <h2>${wonderData.name}</h2>
       <h3><strong>Location:</strong> ${wonderData.location}</h3>
       <p>${wonderData.summary}</p>
       
     `;
-
+    imageContainer.appendChild(imageDetail);
     container.appendChild(cardDetail);
   } else {
     container.innerHTML = "<p>No wonder data found.</p>";
